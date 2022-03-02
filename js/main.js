9a146a237bf9503600d3cb39c55e0576e475bad3
Phil Peng <pp@Phils-MacBook-Air.local>
2022-03-03 05:50:10 +0800

    input data 2x9

--- js/main.js
+++ js/main.js
@@ -9,10 +9,19 @@ $(function() {
 		if ($('#contactForm').length > 0 ) {
 			$( "#contactForm" ).validate( {
 				rules: {
-					patientId: "required",
+					medRecNo: "required",
 					dateOfVisit: "required",
-					odDegree: "required",
-					osDegree: "required",
+					height: "required",
+					od1: "required",
+					os1: "required",
+					od2: "required",
+					os2: "required",
+					od3: "required",
+					os3: "required",
+					od4: "required",
+					os4: "required",
+					od5: "required",
+					os5: "required",
 					// name: "required",
 					// email: {
 					// 	required: true,
@@ -35,7 +44,7 @@ $(function() {
 
 					$.ajax({   	
 				      type: "POST",
-				      url: "https://script.google.com/macros/s/AKfycbz3OUsuq8INIJwUgUXuVQsPVqptM5sS65MoXV8q2HDvf4rNETfKrbf4lXbFJO5mJWQv/exec",
+				      url: "https://script.google.com/macros/s/AKfycbzt33wi50K2OGoRrtzaTFTL7qB4ereZBhGWf0tZXGwfJQIz0VbHXADJkqzUpM_Sk2Bs/exec",
 				      data: $(form).serialize(),
 
 				      beforeSend: function() { 
