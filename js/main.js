1b4702c3f8fca2bc158eaeb9f803b556c94f2ebd
Phil Peng <pp@Phils-MacBook-Air.local>
2022-06-08 21:52:09 +0800

    update table

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ $(function() {
 
 					$.ajax({   	
 				      type: "POST",
-				      url: "https://script.google.com/macros/s/AKfycbzaRtlu1pZ6LUpeUltKTDrB3CB1BrpbKHw8qy-7B2z5tKLQA7938bF6ntDwsEAg-XPg-g/exec",
+				      url: "https://script.google.com/macros/s/AKfycbxtfsXqFryZI0dSz2ADiJHuPkkl2cLc9nDj8eycP9ihOQ6abHn2fUasni0TXu-ogzy4YA/exec",
 				      data: $(form).serialize(),
 
 				      beforeSend: function() { 
