a21cdd2a8ef1f36b64bc437e7d7770d5ebf39715
十百千 <x1001000@users.noreply.github.com>
2022-10-03 13:40:28 +0800

    OD/OS legend

--- plot.py
+++ plot.py
@@ -10,16 +10,11 @@ import matplotlib.pyplot as plt
 def plot(sex):
     morf = data_to_plot.loc[sex].loc[3:16]
     MorF = {'男': 'Male', '女': 'Female'}[sex]
-    plt.xticks(range(3, 17))
-    plt.yticks(range(20, 30))
     plt.fill_between(morf.index, morf['<lambda_4>'], morf['<lambda_3>'], color='red', alpha=0.6, label='90~100%')
     plt.fill_between(morf.index, morf['<lambda_3>'], morf['<lambda_2>'], color='orange', alpha=0.6, label='75~90%')
     plt.fill_between(morf.index, morf['<lambda_2>'], morf['<lambda_1>'], color='yellow', alpha=0.6, label='50~75%')
     plt.fill_between(morf.index, morf['<lambda_1>'], morf['<lambda_0>'], color='lightgreen', alpha=0.6, label='0~50%')
-    plt.legend(loc='lower right')
     plt.title(f'Axial Length Growth in {MorF} Taiwanese Children', fontsize=14)
-    plt.xlabel('Age', fontsize=14)
-    plt.ylabel('Axial Length', fontsize=14)
 
 risk = [...] * 4
 risk[0] = '軸長在年齡正常範圍內，屬無/少風險，建議一年定期檢查，無潛在近視發展狀況。'
@@ -47,7 +42,12 @@ else:
 
 plot(sex)
 if y1:
-    plt.scatter(x, y1, color='red')
+    plt.scatter(x, y1, color='red', label='OD')
 if y2:
-    plt.scatter(x, y2, color='blue')
+    plt.scatter(x, y2, color='blue', label='OS')
+plt.legend(loc='lower right')
+plt.xticks(range(3, 17))
+plt.yticks(range(20, 30))
+plt.xlabel('Age', fontsize=14)
+plt.ylabel('Axial Length', fontsize=14)
 plt
