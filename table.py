676e370542bbf41936660aae87975991e83282fb
Phil Peng <x1001000@gmail.com>
2022-12-07 20:24:35 +0800

    adjust table column size

--- table.py
+++ table.py
@@ -1,25 +1,26 @@
 from js import sex, age, y1, y2, records
 
 import re
-def x(age):
+def _y_m(age):
     m = re.match('(\d+)歲(\d+)', age)
-    return int(m.group(1)) + int(m.group(2)) / 12
+    return f'{m.group(1)}y{m.group(2)}m__'
 
 table = {}
+table['Age__'] = [f'{_y_m(age)}']
 table['OD'] = [f'{y1:.2f}']
 table['OS'] = [f'{y2:.2f}']
-table['Age'] = [f'{x(age):.1f}']
 
 import json
 records = json.loads(records)
-for record in records[::-1]:
+for record in records:
     if record[18] or record[24]:
+        table['Age__'].insert(0, f'{_y_m(record[11])}')
         table['OD'].insert(0, f'{record[18]:.2f}')
         table['OS'].insert(0, f'{record[24]:.2f}')
-        table['Age'].insert(0, f'{x(record[11]):.1f}')
 
 import pandas as pd
 df = pd.DataFrame(table)
 df = df.T
-df.columns = len(df.columns) * ['.......']
+df.columns = df.loc['Age__']
+df = df.drop(index='Age__')
 df
